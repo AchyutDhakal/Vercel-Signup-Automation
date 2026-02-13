457baef0bc93ba77d3c24169bdbccaabe425e55e
Achyut Dhakal <achyutdhakal006@gmail.com>
2026-02-13 09:47:05 +0545

    Connected to jenkins

--- utils/test_runner.py
+++ utils/test_runner.py
@@ -4,6 +4,9 @@ from selenium.webdriver.chrome.options import Options
 def create_driver():
     options = Options()
     options.add_argument("--start-maximized")
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(options=options)
     return driver
